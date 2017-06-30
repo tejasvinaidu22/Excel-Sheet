909af4278cc6014a4448da9d370cc403d8b405f4
tejasvinaidu22 <tejasvinaidu22@gmail.com>
2017-06-30 16:55:00 -0500

    Add files via upload

---
+++ ExampleJavaProject/src/JavaProject1/src/ExceptionExample/UncheckedException.java
@@ -0,0 +1,13 @@
+package ExceptionExample;
+
+public class UncheckedException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0;
+		int y=199;
+		int z=y/x;
+
+	}
+
+}
