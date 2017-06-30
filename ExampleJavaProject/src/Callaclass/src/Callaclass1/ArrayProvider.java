43cc1bca01d3d6e553b980be23c19ab409e97987
tejasvinaidu22 <tejasvinaidu22@gmail.com>
2017-06-30 16:14:38 -0500

    Add files via upload

---
+++ ExampleJavaProject/src/Callaclass/src/Callaclass1/ArrayProvider.java
@@ -0,0 +1,22 @@
+package Callaclass1; 
+
+// https://stackoverflow.com/questions/3861716/how-to-call-a-method-of-a-class-kept-under-other-package
+
+public class ArrayProvider {
+	
+	public  getArray(){
+		//String[] array1 = new String{"I'm", "inside", "an", "array"};
+	// return array1;
+		
+		System.out.println("Package 1);
+	 
+	 }
+	public static  getObject(){
+		//String[] array2 = new String{"I'm", "inside", "an", "array"};
+		//return array2;
+		
+		System.out.println("Package 1; Second method);
+		}
+	}
+
+
---
+++ ExampleJavaProject/src/Callaclass2/src/Callaclassexample/classexample.java
@@ -0,0 +1,20 @@
+package Callaclassexample;
+import Callaclass.ArrayProvider;
+
+public class classexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+		//Object result1= ArrayProvider.getObject();
+		
+		ArrayProvider a = new ArrayProvider();
+		a.getArray();
+		
+		ArrayProvider b = new ArrayProvider();
+		//String[] result2 = provider.getArray();
+		b.getobject();
+
+	}
+
+}
